24297262e44ff88396813a766aa3ce6563b644e8
Samad Rahman~ <samadsrahman@gmail.com>
2024-06-10 11:54:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 
-function App() {
+const App = () => {
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
+  const sources = [
+    'https://bgjokrb8n4my-hls-push.5centscdn.com/videoCampaign/video-1718000030557.mp4/playlist.m3u8',
+    'https://bgjokrb8n4my-hls-push.5centscdn.com/videoCampaign/video-1718000030557.mp4/playlist.m3u8',
+  ];
+  useEffect(() => {
+    const player = playerRef.current;
+
+    if (!player) {
+      const videoElement = videoRef.current;
+
+      if (!videoElement) return;
+
+      playerRef.current = videojs(videoElement, {
+        sources: sources.map(src => ({
+          src,
+          type: 'application/x-mpegURL',
+        })),
+      });
+    }
+
+    return () => {
+      if (player) {
+        player.dispose();
+      }
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-vjs-player>
+      <video ref={videoRef} className="video-js vjs-default-skin" controls crossOrigin='anonymous' />
     </div>
   );
-}
+};
 
 export default App;
